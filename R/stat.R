aa9e56a3ee3837ea5228a87789f15ced2f5a1d63
pedropark99 <pedropark99@gmail.com>
2022-12-23 21:23:28 -0300

    Save last changes

--- R/stat.R
+++ R/stat.R
@@ -8,27 +8,32 @@ calc_percents <- function(x){
 
 
 
-StatTunnel <- ggplot2::ggproto("StatTunnel", ggplot2::Stat,
-                               required_aes = c("x", "y"),
+StatTunnel <- ggplot2::ggproto(
+  "StatTunnel", ggplot2::Stat,
 
-                               setup_params = function(data, params) {
-                                 agg <- aggregate(data$x, list(groups = data$y), sum)
-                                 top <- max(agg$x, na.rm = TRUE)
-                                 params$max.value <- top
-                                 params
-                               },
+  required_aes = c("x", "y"),
 
-                               compute_group = function(data, scales, max.value = 0) {
-                                 total <- sum(data$x, na.rm = TRUE)
-                                 percent <- total / max.value
-                                 data.frame(width = percent, x = 0, y = data$y)
-                               }
+  setup_params = function(data, params) {
+    agg <- aggregate(data$x, list(groups = data$y), sum)
+    top <- max(agg$x, na.rm = TRUE)
+    params$max.value <- top
+    params
+  },
 
+  compute_group = function(data, scales, max.value = 0) {
+    total <- sum(data$x, na.rm = TRUE)
+    percent <- total / max.value
+    data.frame(width = percent, x = 0, y = data$y)
+  }
 )
+
+
 
 stat_tunnel <- function(mapping = NULL, data = NULL, geom = "tile",
                         position = "identity", na.rm = FALSE, show.legend = NA,
                         inherit.aes = TRUE, height = 0.8, ...) {
+
+
   ggplot2::layer(
     stat = StatTunnel, data = data, mapping = mapping, geom = geom,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,

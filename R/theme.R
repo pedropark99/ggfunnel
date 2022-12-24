2c0423e04dedd98a09529b1962891cfec900f1d3
pedropark99 <pedropark99@gmail.com>
2022-12-23 21:09:53 -0300

    Aesthetic changes in code

--- R/theme.R
+++ R/theme.R
@@ -3,7 +3,10 @@
 theme_tunnel <- function() {
   ggplot2::theme_void() +
     ggplot2::theme(
-      axis.title.y = ggplot2::element_text(angle = 90, margin = ggplot2::margin(r = 10)),
+      axis.title.y = ggplot2::element_text(
+        angle = 90,
+        margin = ggplot2::margin(r = 10)
+      ),
       axis.text.y = ggplot2::element_text()
     )
 }
